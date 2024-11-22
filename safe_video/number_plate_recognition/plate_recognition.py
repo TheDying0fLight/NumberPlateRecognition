f689f55d0b6947f387e65809490e80304e84e43f
Kyotoo-chan <ben.tischberger@gmail.com>
2024-11-22 20:07:04 +0100

    added noise

--- safe_video/number_plate_recognition/plate_recognition.py
+++ safe_video/number_plate_recognition/plate_recognition.py
@@ -1,6 +1,7 @@
 import os
 from ultralytics import YOLO
 from ultralytics.engine.results import Boxes
+import cv2
 
 class DetectionResults():
     def __init__(self, boxes, conf, cls, classlegend):
@@ -20,10 +21,18 @@ class NumberPlateRecognition():
         model_path = os.path.join(os.path.abspath("."),"models","first10ktrain","weights","best.onnx")
         self.model = YOLO(model_path, task='detect')
 
-    def analyze(self, image) -> dict:
+    def analyze(self, image):
         result = self.model(image)[0]
         data: Boxes = result.boxes.cpu().numpy()
         return DetectionResults(data.xyxy,data.conf,data.cls,result.names)
 
-    def blur_image(self):
-        print(f'Image at {self.file_path} has to be blurred')
+    def blur_image(self, image, bboxes):
+        int_bboxes = bboxes.astype('int')
+        for box in int_bboxes:
+            cropped_img = image[box[1]:box[3], box[0]:box[2]]
+            blurred_img = cv2.GaussianBlur(cropped_img, (25,25),0)
+            x_offset, y_offset = box[0], box[1]
+            x_end = x_offset + blurred_img.shape[1]
+            y_end = y_offset + blurred_img.shape[0]
+            image[y_offset:y_end, x_offset:x_end] = blurred_img
+        return image
