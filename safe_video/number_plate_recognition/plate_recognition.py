1932323df991807ade98aa71943637570f0f0e03
Simon D. <simon.doehl@googlemail.com>
2024-11-22 13:22:44 +0100

    detection class

--- safe_video/number_plate_recognition/plate_recognition.py
+++ safe_video/number_plate_recognition/plate_recognition.py
@@ -2,19 +2,28 @@ import os
 from ultralytics import YOLO
 from ultralytics.engine.results import Boxes
 
+class DetectionResults():
+    def __init__(self, boxes, conf, cls, classlegend):
+        self.boxes = boxes
+        self.conf = conf
+        self.cls = cls
+        self.clslgd = classlegend
+
+    def __str__(self):
+        ret = ""
+        for a,b in self.__dict__.items(): ret = f"{ret}{a}: {b}\n"
+        return ret
+
 class NumberPlateRecognition():
     def __init__(self, file_path: str = "."):
         self.file_path = file_path
         model_path = os.path.join(os.path.abspath("."),"models","first10ktrain","weights","best.onnx")
         self.model = YOLO(model_path, task='detect')
 
-    def analyze(self, image):
+    def analyze(self, image) -> dict:
         result = self.model(image)[0]
         data: Boxes = result.boxes.cpu().numpy()
-        return {'boxes': data.xyxy,
-                'conf': data.conf,
-                'cls': data.cls,
-                'clsleg': result.names}
+        return DetectionResults(data.xyxy,data.conf,data.cls,result.names)
 
     def blur_image(self):
         print(f'Image at {self.file_path} has to be blurred')
