c6833e5e2f78ac46fad78c06a24f9356f0deb188
Simon D. <simon.doehl@googlemail.com>
2024-11-22 13:08:16 +0100

    return bbs, conf, cls, clsleg

--- safe_video/number_plate_recognition/plate_recognition.py
+++ safe_video/number_plate_recognition/plate_recognition.py
@@ -1,10 +1,20 @@
+import os
+from ultralytics import YOLO
+from ultralytics.engine.results import Boxes
+
 class NumberPlateRecognition():
-
-    def __init__(self, file_path: str):
+    def __init__(self, file_path: str = "."):
         self.file_path = file_path
+        model_path = os.path.join(os.path.abspath("."),"models","first10ktrain","weights","best.onnx")
+        self.model = YOLO(model_path, task='detect')
 
-    def get_bounding_boxes(self):
-        raise NotImplementedError('bounding_boxes not implemented')
+    def analyze(self, image):
+        result = self.model(image)[0]
+        data: Boxes = result.boxes.cpu().numpy()
+        return {'boxes': data.xyxy,
+                'conf': data.conf,
+                'cls': data.cls,
+                'clsleg': result.names}
 
     def blur_image(self):
         print(f'Image at {self.file_path} has to be blurred')
